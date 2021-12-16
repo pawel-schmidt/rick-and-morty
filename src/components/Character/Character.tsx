a02ec8c16b57d7e3262ef331dd5dd12ff8a5ce30
Pawel Schmidt <pawel-schmidt@users.noreply.github.com>
2021-12-16 17:56:25 +0100

    feat: redesign characters grid cell

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import { Character as CharacterType } from '../../generated/graphql'
+import Proportional from '../Proportional/Proportional'
 import { Image, Name, Wrapper } from './styled'
 
 type Props = Pick<CharacterType, 'name' | 'image'>
 
 const Character = ({ name, image }: Props) => (
-  <Wrapper>
-    {image && <Image src={image} alt={name ?? 'Character'} />}
-    <Name>{name}</Name>
-  </Wrapper>
+  <Proportional widthToHeightRatio={0.75}>
+    <Wrapper>
+      {image && <Image src={image} alt={name ?? 'Character'} />}
+      <Name>{name}</Name>
+    </Wrapper>
+  </Proportional>
 )
 
 export default Character
