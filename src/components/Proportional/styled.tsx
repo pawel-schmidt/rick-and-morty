06d599d9edab10154f2825f6d28fe2d353b64353
Pawel Schmidt <pawel-schmidt@users.noreply.github.com>
2021-12-16 17:01:21 +0100

    feat: add Proportional component

---
+++ src/components/Proportional/styled.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 0;
+  position: relative;
+`
+export const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
