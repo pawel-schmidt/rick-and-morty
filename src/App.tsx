198abaaa4c9578155c0f3e7d20a151266815f87e
Pawel Schmidt <pawel-schmidt@users.noreply.github.com>
2021-12-16 08:43:45 +0100

    feat: display character data from API

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import lazy from './utils/lazy'
 
 const Home = lazy(() => import('./pages/Home'))
 const Characters = lazy(() => import('./pages/Characters'))
+const CharacterDetails = lazy(() => import('./pages/CharacterDetails'))
 const Other = lazy(() => import('./pages/Other'))
 const NotFound = lazy(() => import('./pages/NotFound'))
 
@@ -13,7 +14,10 @@ const App = (): React.ReactElement => (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="characters" element={<Characters />} />
+      <Route path="characters">
+        <Route index element={<Characters />} />
+        <Route path=":characterId" element={<CharacterDetails />} />
+      </Route>
       <Route path="other" element={<Other />} />
       <Route path="*" element={<NotFound />} />
     </Route>
