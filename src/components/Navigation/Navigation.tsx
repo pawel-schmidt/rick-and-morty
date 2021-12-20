231195e7216c570acc9ad2b2ed95b64f87a280be
Pawel Schmidt <pawel-schmidt@users.noreply.github.com>
2021-12-20 07:24:49 +0100

    feat: add breakpoints

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
+import { Container } from '../LayoutUtils'
 import NavigationLink from './NavigationLink'
-import { Nav } from './styled'
+import { Nav, Wrapper } from './styled'
 
 const Navigation = () => {
   return (
-    <Nav>
-      <NavigationLink to="/">Home</NavigationLink>
-      <NavigationLink to="/characters">Characters</NavigationLink>
-      <NavigationLink to="/other">Other</NavigationLink>
-    </Nav>
+    <Wrapper>
+      <Container>
+        <Nav>
+          <NavigationLink to="/">Home</NavigationLink>
+          <NavigationLink to="/characters">Characters</NavigationLink>
+          <NavigationLink to="/other">Other</NavigationLink>
+        </Nav>
+      </Container>
+    </Wrapper>
   )
 }
 
